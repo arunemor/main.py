4cafc100427b4727b23a5ed4aaeab410434710e9
arunemor <cloudemor2025@gmail.com>
2025-05-23 18:15:33 +0530

    modify the code add flask

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-# hello_emorphis.py
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
 def greet():
-    print("Hello Emorphis!")
+    return "Hello Emorphis!"
 
 if __name__ == "__main__":
-    greet()
+    app.run(host="0.0.0.0", port=5000)
